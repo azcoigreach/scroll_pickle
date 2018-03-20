4f67b66467df5fbe4138a6be744fde1886fe78e0
azcoigreach <azcoigreach@gmail.com>
2018-03-20 13:08:41 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    author="azcoigreach",
+    author_email="azcoigreach@gmail.com",
+    name = 'Wordcloud Maker',
+    version = '0.2.4',
+    py_modules = ['wordcloud_maker'],
+    install_requires = [
+        'click',
+        'colorama',
+        'coloredlogs',
+        'pymongo',
+        'matplotlib',
+        'pillow',
+        'wordcloud',
+        'pyfiglet',
+        'numpy',
+    ],
+    entry_points = '''
+        [console_scripts]
+        wordcloud_maker=wordcloud_maker:main
+    ''',
+)
