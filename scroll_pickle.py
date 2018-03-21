17a9e8a1f6b94c16840bb3201d8863a6f57b4140
azcoigreach <azcoigreach@gmail.com>
2018-03-20 17:44:39 -0700

    debug multiline processing

--- scroll_pickle.py
+++ scroll_pickle.py
@@ -49,28 +49,25 @@ def main(debug, file):
     lines = []
     logger.debug('p_data type: %s',type(p_data))
     
-    # try:
-    #     lines = []
-    #     for i in iter(p_data):
-    #         for key, value in i.items():
-    #             if key == '_id':
-    #                 id_value = value
-    #                 # logger.debug(id_value)
-    #             if key == 'count':
-    #                 count_value = value
-    #                 # logger.debug(count_value)
+    try:
+        logger.debug('multi-line')
+        for i in iter(p_data):
+            for key, value in i.items():
+                if key == '_id':
+                    id_value = value
+                    # logger.debug(id_value)
+                if key == 'count':
+                    count_value = value
+                    # logger.debug(count_value)
 
-    #         data = str(id_value + count_value)
-    #         logger.debug('data type: %s', type(data))
-    #         logger.debug(data)
-    #         lines.append(data)
-    #         logger.debug('multi-line')
-    # except:
-   
-    # logger.debug('single line')
-    # logger.debug()
-    lines.append(str(p_data))
-    
+            data = str(id_value + count_value)
+            logger.debug('data: [%s] %s', type(data), data)
+            lines.append(data)
+            
+    except:
+        logger.debug('single line')
+        lines.append(str(p_data) + ' ')
+        
     logger.debug('words list: [%s] %s', type(lines), lines)
 
     # Uncomment to rotate 180 degrees
