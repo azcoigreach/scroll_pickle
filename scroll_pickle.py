a2944e39267e8f91876e24c73d19ca6e2ebb75f3
azcoigreach <azcoigreach@gmail.com>
2018-03-20 17:25:15 -0700

    debug

--- scroll_pickle.py
+++ scroll_pickle.py
@@ -66,7 +66,7 @@ def main(debug, file):
             lines.append(data)
             logger.debug('multi-line')
     except:
-        lines = lines.insert(str(p_data))
+        lines = lines.insert(str(p_data), 0)
         logger.debug('single line')
     logger.debug('words list: [%s] %s', type(lines), lines)
 
