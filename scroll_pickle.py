57867e803431f898a59f38e200d49c8a66e206b1
azcoigreach <azcoigreach@gmail.com>
2018-03-20 17:52:08 -0700

    debug

--- scroll_pickle.py
+++ scroll_pickle.py
@@ -60,7 +60,7 @@ def main(debug, file):
                     count_value = value
                     # logger.debug(count_value)
 
-            data = str(id_value + count_value)
+            data = str(id_value + ' : ' + count_value)
             logger.debug('data: [%s] %s', type(data), data)
             lines.append(data)
             
